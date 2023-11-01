419423c390a8bb6e669381cb0880259d4c59f842
zhv-oo <zhitenev.oleg@yandex.ru>
2023-11-01 22:28:11 +0700

    добавил менеджера очередей

--- dispatcher/src/main/java/ru/openres/services/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/ru/openres/services/impl/UpdateProducerImpl.java
@@ -1,6 +1,7 @@
 package ru.openres.services.impl;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.openres.services.UpdateProducer;
@@ -8,8 +9,15 @@ import ru.openres.services.UpdateProducer;
 @Service
 @Log4j
 public class UpdateProducerImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void producer(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
